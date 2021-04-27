f6bfcb331a07f7132b728257be0d30ec3595b3e6
BillKalin <xjl125521@163.com>
2021-04-27 15:00:52 +0800

    optimize native crash dump code

--- breakpad/src/main/jni/native_crash_handler.cpp
+++ breakpad/src/main/jni/native_crash_handler.cpp
@@ -4,14 +4,20 @@
 
 #include <jni.h>
 #include <string>
-
+#include "android/log.h"
 #include "client/linux/handler/exception_handler.h"
 #include "client/linux/handler/minidump_descriptor.h"
 
-bool DumpCallback(const google_breakpad::MinidumpDescriptor &descriptor,
+#define TAG "native_crash_handler"
+
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+
+using namespace google_breakpad;
+
+bool DumpCallback(const MinidumpDescriptor &descriptor,
                   void *context,
                   bool succeeded) {
-    printf("Dump path: %s\n", descriptor.path());
+    LOGD("DumpCallback success, path = %s, is success = %d\n", descriptor.path(), succeeded);
     return succeeded;
 }
 
@@ -23,16 +29,25 @@ void Crash() {
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_billkalin_breakpad_NativeCrashHandler_init(JNIEnv
-*env,jobject thiz, jstring dump_path) {
+Java_com_billkalin_breakpad_NativeCrashHandler_init(JNIEnv *env, jobject thiz, jstring dump_path) {
     const char *path = env->GetStringUTFChars(dump_path, 0);
-    google_breakpad::MinidumpDescriptor descriptor(path);
-    google_breakpad::ExceptionHandler eh(descriptor, NULL, DumpCallback,
-                                     NULL, true, -1);
+    LOGD("NativeCrashHandler.init: dump path = %s\n", path);
+    MinidumpDescriptor descriptor(path);
+    static ExceptionHandler eh(descriptor, NULL, DumpCallback,
+                               NULL, true, -1);
+    env->ReleaseStringUTFChars(dump_path, path);
 }
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_billkalin_breakpad_NativeCrashHandler_testCrash(JNIEnv *env, jobject thiz) {
     Crash();
+}
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
+    JNIEnv *env;
+    if ((vm->GetEnv((void **) &env, JNI_VERSION_1_6)) != JNI_OK)
+        return JNI_ERR;
+    LOGD("JNI_OnLoad()");
+    return JNI_VERSION_1_6;
 }
